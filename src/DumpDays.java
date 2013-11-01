3f0cfc8c90cf7206e169a5959a8d55229b85b2ed
Nagesh Potdar <nppotdar@gmail.com>
2013-11-01 09:59:56 -0400

    add days from code

---
+++ src/DumpDays.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import org.joda.time.Days;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+
+public class DumpDays {
+	
+	public void readAndDumpCsvFile(String inputFileName, String dumpFileName, int columnNumber) throws Exception{
+		BufferedReader in = new BufferedReader(new FileReader(new File(inputFileName)));
+		CSVWriter outCsv = new CSVWriter(new PrintWriter(dumpFileName));
+		String line[];
+		String lineString;
+		int id = 1;
+		DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime endDateTime = DateTimeFormat.forPattern("yyyy-MM-dd").parseLocalDateTime("2013-04-30"); 
+		in.readLine();
+		while( (lineString = in.readLine()) != null ){
+			line = lineString.split("(\",\")");
+			LocalDateTime currentDateTime = null;
+			try {
+				currentDateTime = dateTimeFormatter.parseLocalDateTime(line[columnNumber].trim());
+			} catch (Exception e1) {
+				try {
+					currentDateTime = dateTimeFormatter.parseLocalDateTime(line[line.length-2].trim());
+				} catch (Exception e2) {
+					System.out.println("Error at line: ");
+					System.out.println("line " + id + "\n" + Arrays.toString(line));
+				}
+			}
+			Days diff = null;
+			try {
+				diff  = Days.daysBetween(endDateTime, currentDateTime);
+				outCsv.writeNext(new String[]{String.valueOf(id), String.valueOf(-1*diff.getDays())});
+			} catch (Exception e) {
+				System.out.println("\n*\n*\n*\n*\nCheck line " + id  + "\n" + line.length + "\n" + Arrays.toString(line));
+				System.out.println("Cell data: " + line[columnNumber]);
+			}
+			id++;
+		}
+		in.close();
+		outCsv.close();
+	}	
+	public static void main(String[] args) throws Exception{
+		//term frequency in the description tag
+		//term frequency in the Document as a whole
+		new DumpDays().readAndDumpCsvFile("./res/train.csv", "./res/days_feature.csv", 9);
+		
+	}
+
+}
--- src/DumpTFIDF.java
+++ src/DumpTFIDF.java
@@ -7,15 +7,17 @@ import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
 
 
-public class ReadAndDumpCSV {
+public class DumpTFIDF {
 	public void readAndDumpCsvFile(String inputFileName, String dumpFileName, String wordFreqFileName, int columnNumber) throws Exception{
 		CSVReader in = new CSVReader(new FileReader(new File(inputFileName)));
 		PrintWriter out = new PrintWriter(new File(dumpFileName)); 
 		CSVWriter outCsv = new CSVWriter(new PrintWriter(wordFreqFileName));
-		HashMap<String, Integer> freqCount = new HashMap<String, Integer>();
+		
+		HashMap<String, Integer> freqCount = new HashMap<String, Integer>();		
 		String line[];
 		String lineTokens[];
 		in.readNext();
+		
 		while( (line = in.readNext()) != null){
 			lineTokens = line[columnNumber].split("[\\s\\,\\.\\-\\\\/]");
 			out.println(line[columnNumber]);
@@ -27,8 +29,24 @@ public class ReadAndDumpCSV {
 				freqCount.put(word, freqCount.get(word)+1);
 			}
 		}
-		for(String key: freqCount.keySet())
-			outCsv.writeNext(new String[]{key, freqCount.get(key).toString()});
+		in = new CSVReader(new FileReader(new File(inputFileName)));
+		in.readNext();
+		int id = 1;
+		while( (line = in.readNext()) != null){	
+			if(id == 493)
+				System.out.println("Hoi");
+			lineTokens = line[columnNumber].split("[\\s\\,\\.\\-\\\\/]");
+			double answer = 0.0;
+			for(String word: lineTokens){
+				word = cleanWord(word);
+				if(word== null )continue;
+				answer += freqCount.get(word)*1.0/(lineTokens.length);
+			}
+			outCsv.writeNext(new String[]{String.valueOf(id++), String.valueOf(answer)});
+			if(id == 494)
+				System.out.println("Hoi");
+		}
+		
 		in.close();
 		out.close();
 		outCsv.close();
@@ -41,12 +59,12 @@ public class ReadAndDumpCSV {
 		i = wordObj.length()-1;
 		while(i >= 0 && wordObj.length() > 0 && (wordObj.charAt(i) <'a' || wordObj.charAt(i) > 'z'))
 			wordObj.deleteCharAt(i--);
-		if(wordObj.length() <= 0)
+		if(wordObj.length() <= 2)
 			return null;
 		return wordObj.toString();
 	}
 	public static void main(String args[]) throws Exception{
-		ReadAndDumpCSV obj = new ReadAndDumpCSV();
+		DumpTFIDF obj = new DumpTFIDF();
 		obj.readAndDumpCsvFile("./res/train.csv", "./res/dumped.txt", "./res/wordFreq.csv", 4);
 	}
 }
